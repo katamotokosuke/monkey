ffaced103bfa04d23cc53815d06e6a11a33075e6
KosukeKatamoto <katamoto_kosuke@uniquevision.co.jp>
2018-08-04 10:33:20 +0900

    Extend repl

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -1,11 +1,12 @@
 package repl
 
 import (
-	"io"
 	"bufio"
 	"fmt"
+	"io"
 	"lexer"
-	"token"
+	"parser"
+	"evaluator"
 )
 
 const PROMPT = ">> "
@@ -15,16 +16,47 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Printf(PROMPT)
-
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
+
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
